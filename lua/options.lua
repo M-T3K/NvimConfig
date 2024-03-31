190d571f5f42aa9e672a43e33dc996b3aab9e22a
M-T3K <kevinalb12@gmail.com>
2024-03-31 23:06:34 +0200

    Updated some settings

--- lua/options.lua
+++ lua/options.lua
@@ -2,8 +2,12 @@
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
 
+-- Get Fatty Cursor on Insert Mode
+vim.opt.guicursor = 'a:block-blinkon100-Cursor'
+
 -- Set highlight on search
-vim.o.hlsearch = true
+vim.o.hlsearch = false
+vim.o.incsearch = true
 
 -- Make line numbers default
 vim.wo.number = true
@@ -17,6 +21,20 @@ vim.o.mouse = 'a'
 --  See `:help 'clipboard'`
 vim.o.clipboard = 'unnamedplus'
 
+-- Indenting Stuff
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.o.smartindent = true
+
+-- When selecting a line near the bottom of the viewport, ensure a minimum of 8 lines are still shown under, unless there are no lines left (bottom of file)
+vim.o.scrolloff = 8
+
+-- No line wrap because it sucks
+vim.o.wrap = false
+
 -- Enable break indent
 vim.o.breakindent = true
 
@@ -27,11 +45,14 @@ vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+-- Mix Signs and Line Numbers
+vim.wo.signcolumn = 'number'
+
+-- Cursor Line
+vim.wo.cursorline = true
 
 -- Decrease update time
-vim.o.updatetime = 250
+vim.o.updatetime = 50
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
