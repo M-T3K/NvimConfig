5575b9e88a14eebfab72960f58b7d96bf5161fe8
M-T3K <kevinalb12@gmail.com>
2024-04-17 23:53:25 +0200

    set bind \ to exit normal mode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,8 @@
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>vs', vim.cmd.vsplit)
+vim.keymap.set('i', '\\\\', '<ESC>') --Remap to exit insert mode back to the normal mode
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
